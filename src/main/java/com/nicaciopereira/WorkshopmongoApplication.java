7bdca4826d1aaf9bcf05fdcd9e82b9ca9b54148e
nicacio pereira <vd829020@gmail.com>
2023-11-29 23:15:48 -0300

    Project created

---
+++ src/main/java/com/nicaciopereira/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.nicaciopereira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        SpringApplication.run(WorkshopmongoApplication.class,args);
+    }
+
+}
